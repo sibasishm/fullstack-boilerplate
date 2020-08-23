a561c9057c3f53b097211dfdd4bf05ebe122cb14
Sibasish Mohanty <smsibasish@gmail.com>
2020-08-23 19:33:32 +0530

    creating first entity

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import { MikroORM } from '@mikro-orm/core';
+import { Quote } from './entities/Quote';
 
 const main = async () => {
 	const orm = MikroORM.init({
+        entities: [Quote]
 		dbName: 'quotebook',
 		type: 'postgresql',
 		debug: process.env.NODE_ENV !== 'production',
