bc83e55eb0cce47d2dfcde7fdba2d2619be21bbd
Sibasish Mohanty <smsibasish@gmail.com>
2020-08-25 15:20:08 +0530

    auth resolvers added

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 -   postgreSQL as the database.
 -   grahql as the API.
 -   apollo and express as the server.
+-   argon2 for password hasing and not bcrypt.js because of [this reason](https://security.stackexchange.com/questions/193351/in-2018-what-is-the-recommended-hash-to-store-passwords-bcrypt-scrypt-argon2).
--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -4,13 +4,14 @@ import path from 'path';
 
 import { MikroORM } from '@mikro-orm/core';
 import { Quote } from './entities/Quote';
+import { User } from './entities/User';
 
 export default {
 	migrations: {
 		path: path.join(__dirname, './migrations'),
 		pattern: /^[\w-]+\d+\.[tj]s$/,
 	},
-	entities: [Quote],
+	entities: [Quote, User],
 	dbName: 'quotebook',
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
